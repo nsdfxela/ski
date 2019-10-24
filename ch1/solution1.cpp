39317524fb2cf1cc3eb75273261059e8d5dc143d
nsdfxela <nsdfxela@gmail.com>
2019-10-24 18:37:37 +0100

    evening-updates

--- ch1/solution1.cpp
+++ ch1/solution1.cpp
@@ -7,7 +7,7 @@ int count(int n) {
   int nn = n;
   int cnt = 0;
   while(nn > 1) {
-    if(nn % 2 == 0){
+   if(nn % 2 == 0){
       nn/=2;
       cnt++;
     } else {
@@ -23,13 +23,16 @@ int main(void) {
   int i,j;
     int maxElement = 0;
     std::vector <int> results;
-    /*#if 0
-    std::ifstream istr ("test.txt", std::ios::in);
-    while(istr >> i >> j) {
-    #elif*/
+    //#if 0
+    //std::ifstream istr ("test.txt", std::ios::in);
+    //while(istr >> i >> j) {
+      //#elif*/
     while(std::cin >> i >> j){
       //#endif
-    for(int t = i; t <= j; t++) {
+      int mi = i;
+      int mj = j;
+    if(mi > mj) std::swap(mi,mj);
+    for(int t = mi; t <= mj; t++) {
       int ct = count(t);
       if(ct > maxElement) {
 	maxElement = ct;
@@ -38,8 +41,9 @@ int main(void) {
     results.push_back(i);
     results.push_back(j);
     results.push_back(maxElement);
-    }
     maxElement = 0;
+    }
+
         for(int idx = 0; idx < results.size(); idx+=3){
 	  if(idx) std::cout << std::endl;
 	  std::cout << results[idx] << " " << results[idx+1] << " " << results[idx+2];
