a075f582f05d5dd804b94732bb5b1ea16fb3101a
Alexander Fedosin <alexander.fedosin@itseez3d.com>
2019-10-29 16:56:32 +0300

    read-input

--- ch1/solution2.cpp
+++ ch1/solution2.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
 #include <fstream>
+
+#define M 100
+#define N 100
+char field [M][N];
+
+void print_field(int m, int n){
+  for(int i = 0; i < m; i++) {
+    for(int j = 0; j < n; j++){
+      std::cout << field[i][j] << " ";
+    }
+    std::cout << std::endl;
+  }
+}
 int main (int argc, char **argv) {
   std::ifstream istr ("test.txt", std::ios::in);
-  int input = 14;
-  istr >> input;
-  std::cout << input;
+  int m=0, n=0;
+  istr >> m >> n;
+  std::cout << m << " " << n << std::endl;
+  for(int i = 0; i < m; i++) {
+    for(int j = 0 ; j < n; j++){
+      char c;
+      istr >> c;
+      field[i][j] = c;
+    }
+  }
+  print_field(m, n);
   return 0;
 }
