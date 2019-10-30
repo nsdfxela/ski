f9ff520164e4860eb75bbe44ed1dee416dbf8d5e
Alexander Fedosin <alexander.fedosin@itseez3d.com>
2019-10-30 16:53:31 +0300

    initial-s3

--- ch1/solution3.cpp
+++ ch1/solution3.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 #include <fstream>
 
+double payments[1000];
+int read_data(std::istream &stream, int &number){
+  stream >> number;
+  std::cout << number;
+  for(int i = 0 ; i < number; i++) {
+    stream >> payments[i];
+    std::cout << payments[i] << " ";
+  }
+  return number;
+}
+
 int main(int argc, char ** argv) {
   std::ifstream istr ("test.txt", std::ios::in);
+  int studs = 0;
+  while(read_data(istr, studs)) {
+    std::cout << studs;
+  }
+  
 
   return 0;
 }
