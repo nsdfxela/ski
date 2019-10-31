43b82dcd862d62d6e141f587bc41f0c91a26512f
Alexander Fedosin <alexander.fedosin@itseez3d.com>
2019-10-31 14:57:22 +0300

    small-updates

--- ch1/solution3.cpp
+++ ch1/solution3.cpp
@@ -1,22 +1,42 @@
 #include <iostream>
+#include <numeric>
 #include <fstream>
 
 double payments[1000];
 int read_data(std::istream &stream, int &number){
+
   stream >> number;
-  //std::cout << number;
+  std::cout << number;
   for(int i = 0 ; i < number; i++) {
     stream >> payments[i];
-    //std::cout << payments[i] << " ";
+    std::cout << payments[i] << " ";
   }
   return number;
 }
 
+void calculate(int n) {
+  if(!n) return;
+  double sum = std::accumulate(payments, payments + n, 0.0);
+  //std::cout << sum << std::endl;
+  double avg = sum / n;
+  double lsum = std::accumulate(payments, payments + n, 0.0,[avg](double lower_sum, double element){
+					    if(element < avg)
+					      return lower_sum + element;
+					    else
+					      return lower_sum;
+					  });
+  std::cout <<"ls: " << lsum << std::endl;
+  
+  
+}
+
 int main(int argc, char ** argv) {
+  
   std::ifstream istr ("test.txt", std::ios::in);
   int studs = 0;
   while(read_data(istr, studs)) {
-    
+    calculate(studs);
+    std::cout << studs;
   }
   
 
