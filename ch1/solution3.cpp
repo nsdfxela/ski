026410d22c4d356e37583a99878397c3358b1493
Alexander Fedosin <alexander.fedosin@itseez3d.com>
2019-11-01 15:16:35 +0300

    ch1-ex3-accepted!

--- ch1/solution3.cpp
+++ ch1/solution3.cpp
@@ -16,31 +16,47 @@ int read_data(std::istream &stream, int &number){
   return number;
 }
 
+double calculate_int(int n) {
+  if(!n) return 0.0;
+  int sum = 0;
+  for(int i = 0; i < n; i++) {
+    sum+=(int)(payments[i]*100.0);
+  }
+  return 0.0;
+}
+
 double calculate(int n) {
   if(!n) return 0.0;
   double sum = std::accumulate(payments, payments + n, 0.0);
   double avg = std::round(sum / n * 100.0) / 100.0;
   //  std::cout << avg << std::endl;
 
-  double lsum = std::accumulate(payments, payments + n, 0.0,[avg](double lower_sum, double element){
-					    if(element < avg)
-					      return lower_sum + (avg - element);
+  double msum = std::accumulate(payments, payments + n, 0.0,[avg](double lower_sum, double element){
+					    if(element > avg)
+					      return lower_sum + (element - avg);
 					    else
 					      return lower_sum;
 					  });
+  double lsum = std::accumulate(payments, payments + n, 0.0,[avg](double lower_sum, double element){
+					    if(element < avg)
+					      return lower_sum + (avg-element);
+					    else
+					      return lower_sum;
+					  });
+
   //  std::cout <<"ls: " << lsum << std::endl;
-  return lsum;
+  return lsum < msum ? lsum : msum;
   
   
 }
 
 int main(int argc, char ** argv) {
   
-  std::ifstream istr ("test.txt", std::ios::in);
+  //  std::ifstream istr ("test.txt", std::ios::in);
   int studs = 0;
   std::cout <<   std::setprecision(2) << std::fixed;
 
-  while(read_data(istr, studs)) {
+  while(read_data(std::cin, studs)) {
     std::cout << '$' << calculate(studs) << std::endl;
     //    std::cout << studs;
   }
