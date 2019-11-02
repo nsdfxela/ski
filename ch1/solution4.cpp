66a1cd661e8642f0b8decb56153638c8d08a5f8d
nsdfxela <nsdfxela@gmail.com>
2019-11-02 15:06:51 +0100

    revision

--- ch1/solution4.cpp
+++ ch1/solution4.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 int read_data(int &s, int &n, std::istream &st) {
   st >> s;
   st >> n;
   return s;
 }
+
+
+void print_data(const std::string &data, int s) {
+  for(int i = 0; i < data.size(); i++) {
+    std::cout << data[i];
+  }
+}
+
 int main (void) {
   int s = 0, n = 0;
   std::ifstream fstr ("test.txt", std::ios::in);
   while(read_data(s, n, fstr)){
-    std::cout << s << std::endl;
+    //    std::cout << s << std::endl;
+    std::string strn = std::to_string(n);
+    print_data(strn, s);
   }
   return 0;
 }
