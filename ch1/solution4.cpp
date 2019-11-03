04a2b028b7c4eb0c6636ef9a13290f64213bea3a
nsdfxela <nsdfxela@gmail.com>
2019-11-03 12:27:45 +0000

    segments-implementation

--- ch1/solution4.cpp
+++ ch1/solution4.cpp
@@ -7,10 +7,88 @@ int read_data(int &s, int &n, std::istream &st) {
   return s;
 }
 
+#define H (2*s+3)
+#define W (s+2)
+
+#define VSX (W-1)
+#define LVSX (0)
+
+void v1(char *f, int s) {
+  int xspos = VSX;//x start pos
+  int yspos = 1;//y start pos
+  for(int i=yspos;i<s+yspos;i++){ f[i*W + xspos] = '|';}
+}
+
+void v2(char *f, int s) {
+  int xspos = VSX;//x start pos
+  int yspos = 1+s+1;//y start pos
+  for(int i=yspos;i<s+yspos;i++){ f[i*W + xspos] = '|';}
+}
+
+void lv1(char *f, int s) {
+  int xspos = LVSX;//x start pos
+  int yspos = 1;//y start pos
+  for(int i=yspos;i<s+yspos;i++){ f[i*W + xspos] = '|';}
+}
+
+void lv2(char *f, int s) {
+  int xspos = LVSX;//x start pos
+  int yspos = 1+s+1;//y start pos
+  for(int i=yspos;i<s+yspos;i++){ f[i*W + xspos] = '|';}
+}
+
+
+void h1(char *f, int s) {
+  int xspos = 1;//x start pos
+  int yspos = 0;//y start pos
+  for(int i=0;i<s;i++){ f[yspos*W + xspos+i] = '_';}
+}
+void h2(char *f, int s) {
+  int xspos = 1;//x start pos
+  int yspos = 0+s+1;//y start pos
+  for(int i=0;i<s;i++){ f[yspos*W + xspos+i] = '_';}
+}
+void h3(char *f, int s) {
+  int xspos = 1;//x start pos
+  int yspos = 0+s+1+s+1;//y start pos
+  for(int i=0;i<s;i++){ f[yspos*W + xspos+i] = '_';}
+}
+
+
+void print_field(char *data, int s) {
+  for(int i = 0;i<H;i++){
+    for(int j = 0;j<W;j++){
+      std::cout << data[i*W+j];
+    }
+    std::cout << std::endl;
+  }
+
+}
+
+void print1(char * field, int s) {
+  v1(field, s);
+  v2(field, s);
+}
+
+void print2(char * field, int s) {
+  h1(field, s);
+  v2(field, s);
+  h2(field, s);
+  h3(field, s);
+  lv1(field, s);
+}
+
 
 void print_data(const std::string &data, int s) {
   for(int i = 0; i < data.size(); i++) {
-    std::cout << data[i];
+    //    std::cout << data[i];
+    if(data[i] == '1') {
+      char *field = new char[H*W];
+      memset(field, '.', H*W);
+      print2(field, s);
+      print_field(field, s);
+    }
+    
   }
 }
 
