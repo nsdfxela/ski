3fec7ac66f11cf8d89ec86cafc565e73a3c2fc50
Alexander Fedosin <alexander.fedosin@itseez3d.com>
2019-11-07 16:30:34 +0300

    try-read

--- ch1/solution5.cpp
+++ ch1/solution5.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <fstream>
+
+void read_command(std::istream &strm) {
+  char cmd;
+  strm >> cmd;
+  switch(cmd){
+  case 'I' : std::cout << 'I';
+  default: break;
+  }
+}
 
 int main (void) {
-
+  std::ifstream ifstr ("test.txt", std::ios::in);
+  read_command(ifstr);
   return 0;
 }
