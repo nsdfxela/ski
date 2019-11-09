24fdada7763859b93d3c3d2c242aad7a1db48567
nsdfxela <nsdfxela@gmail.com>
2019-11-09 12:59:32 +0000

    s-and-i-commands

--- ch1/solution5.cpp
+++ ch1/solution5.cpp
@@ -1,17 +1,52 @@
 #include <iostream>
+#include <string>
 #include <fstream>
+#include <algorithm>
 
-void read_command(std::istream &strm) {
+char ** image = nullptr;
+int M=-1, N=-1;
+
+void S(std::istream &st) {
+  std::string file_name;
+  st >> file_name;
+  std::cout << file_name << std::endl;
+  for(int i = 0; i < N; i++){
+      for(int j = 0; j < M; j++){
+      	std::cout << image[i][j];
+      }
+      std::cout << std::endl;
+  }
+}
+void I(std::istream &st) {
+  int m, n; //m - columns n -rows
+  st >> m >> n;
+  image = new char*[n];
+  M = m; N = n;
+  for(int i = 0; i < n; i++){
+    image[i]=new char [m];
+    std::fill(image[i], image[i]+m, 'O');
+  }
+
+			    
+}
+
+char read_command(std::istream &strm) {
   char cmd;
   strm >> cmd;
+  std::cout << cmd << std::endl;
   switch(cmd){
-  case 'I' : std::cout << 'I';
-  default: break;
+  case 'I' : I(strm); break;
+  case 'S' : S(strm); break;
+  default:
+    std::string tmp;
+    std::getline(strm, tmp);
+    break;
   }
+  return cmd;
 }
 
 int main (void) {
   std::ifstream ifstr ("test.txt", std::ios::in);
-  read_command(ifstr);
+  while(read_command(ifstr)!='X');
   return 0;
 }
