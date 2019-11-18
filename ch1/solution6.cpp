12b526b2a386179a0ccf90f6dfb7f14d1927cf63
Alexander Fedosin <alexander.fedosin@itseez3d.com>
2019-11-18 10:53:04 +0300

    small-updates

--- ch1/solution6.cpp
+++ ch1/solution6.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <fstream>
 
 #define L 1000
 #define S 3
@@ -8,7 +10,12 @@ int mem [L][S];
 int regs [10];
 int *cur=nullptr;
 void read_blocks(std::istream &istr) {
-  
+  std::string ln;
+  while(getline(istr, ln)){
+    if(ln == "") std::cout << "empty ";
+    std::cout << ln << std::endl;
+
+  }
 }
 void clear_mem() {
   std::fill(regs, regs+R, 0);
@@ -21,7 +28,8 @@ void clear_mem() {
 }
 
 int main(void) {
-  std::istream &istr = std::cin;
+  //  std::istream &istr = std::cin;
+  std::ifstream ifstr ("test.txt", std::ios::in);   std::istream &istr = ifstr;
   int n_of_blocks = 0;
   istr >> n_of_blocks;
   for(int i = 0; i < n_of_blocks; i++) {
