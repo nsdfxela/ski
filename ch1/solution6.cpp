87c46b886ebd96a17776dfc5b81707fdd92e3d05
nsdfxela <nsdfxela@gmail.com>
2019-11-19 18:51:16 +0000

    simple-ops

--- ch1/solution6.cpp
+++ ch1/solution6.cpp
@@ -5,12 +5,12 @@
 
 #define L 1000
 #define S 3
-#define R 10
+#define Rn 10
 
 int mem [L][S];
 int regs [10];
 int cur=0;
-void read_blocks(std::istream &istr) {
+void read_block(std::istream &istr) {
   std::string ln;
   bool block_started = false;
   while(getline(istr, ln)){
@@ -41,13 +41,36 @@ void p_debug(int count = -1)
 
 void clear_mem() {
 
-  std::fill(regs, regs+R, 0);
+  std::fill(regs, regs+Rn, 0);
   for(int i = 0; i < L; i++){
     for(int j = 0; j < S; j++) {
       mem [i][j] = 0;
     }
   }
   cur = 0;
+}
+
+inline int& M(int p){ return  mem[cur][p];}
+inline int& R(int p) {return regs[p];}
+
+bool is_end(){
+  return mem[cur][0] == 1 && mem[cur][1] == 0 && mem[cur][2] == 0;
+}
+
+void exec_cmd() {
+  switch(M(0))
+  {
+  case 2: R(M(1)) = M(2); break;
+  case 3: R(M(1)) += M(3); break;
+      default: break;
+  }
+}
+
+void exec_programm(){
+  do{
+    
+    cur++;
+  } while (!is_end());
 }
 
 int main(void) {
@@ -57,7 +80,7 @@ int main(void) {
   istr >> n_of_blocks;
   for(int i = 0; i < n_of_blocks; i++) {
     clear_mem();
-    read_blocks(istr);
+    read_block(istr);
   }
   p_debug(30);
   return 0;
