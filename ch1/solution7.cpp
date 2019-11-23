3fa9605d5f95b22789aebddc856c0967b149cc93
nsdfxela <nsdfxela@gmail.com>
2019-11-23 18:43:12 +0000

    intermediate-work

--- ch1/solution7.cpp
+++ ch1/solution7.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+
+//bool read8
 int main(void) {
 
   //  std::istream &istr = std::cin;
   std::fstream istr ("test.txt", std::ios::in);
-  
+  std::string ln;
+  while(getline(istr, ln)){
+    std::cout << ln << std::endl;
+  }
   return 0;
 }
