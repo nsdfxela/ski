f5bf58206e014825de731e4417cb73a1b2c644fa
nsdfxela <nsdfxela@gmail.com>
2019-11-24 08:15:31 +0000

    initial-work-on-7

--- ch1/solution7.cpp
+++ ch1/solution7.cpp
@@ -2,14 +2,32 @@
 #include <fstream>
 #include <string>
 
-//bool read8
+const int N = 8;
+char chess_board [N][N];
+
+
+bool read8(std::istream &istr) {
+
+  bool is_empty = true;
+  for(int i = 0; i < N;i++){
+    for(int j =0; j < N; j++) {
+      istr >> chess_board[i][j];
+      std::cout << chess_board[i][j];
+      is_empty = is_empty && (chess_board[i][j] == '.');
+    }
+    std::cout << '\n';
+  }
+  std::cout << '\n';
+  return !is_empty;
+}
 int main(void) {
 
   //  std::istream &istr = std::cin;
   std::fstream istr ("test.txt", std::ios::in);
   std::string ln;
-  while(getline(istr, ln)){
-    std::cout << ln << std::endl;
+  while(read8(istr)){
+    
   }
+
   return 0;
 }
