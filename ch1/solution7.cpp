ae7c22d62c96c39be90b991f736fdb563e301697
nsdfxela <nsdfxela@gmail.com>
2019-11-24 08:23:36 +0000

    updates

--- ch1/solution7.cpp
+++ ch1/solution7.cpp
@@ -5,6 +5,12 @@
 const int N = 8;
 char chess_board [N][N];
 
+int w_k_pos_x = -1;
+int w_k_pos_y = -1;
+int b_k_pos_x = -1;
+int b_k_pos_y = -1;
+
+
 
 bool read8(std::istream &istr) {
 
@@ -12,12 +18,20 @@ bool read8(std::istream &istr) {
   for(int i = 0; i < N;i++){
     for(int j =0; j < N; j++) {
       istr >> chess_board[i][j];
-      std::cout << chess_board[i][j];
+      //std::cout << chess_board[i][j];
       is_empty = is_empty && (chess_board[i][j] == '.');
+      if(chess_board[i][j] == 'k') {
+	b_k_pos_x = j;
+	b_k_pos_y = i;
+      }
+      if(chess_board[i][j] == 'K') {
+	w_k_pos_x = j;
+	w_k_pos_y = i;
+      }
     }
-    std::cout << '\n';
+    //    std::cout << '\n';
   }
-  std::cout << '\n';
+  //  std::cout << '\n';
   return !is_empty;
 }
 int main(void) {
