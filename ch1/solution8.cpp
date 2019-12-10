bdf1d585386f36b3ca3ecd39338df316f02942b7
Alexander Fedosin <alexander.fedosin@itseez3d.com>
2019-12-10 12:15:25 +0300

    udpates

--- ch1/solution8.cpp
+++ ch1/solution8.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+
+char cand[20][80];
+void read_data(std::istream &istr) {
+  std::string buf;
+  std::getline(istr, buf); //empty
+  int ncand = 0;
+  istr >> ncand;
+  std::cout << ncand << std::endl;
+  
+}
 
 int main (void) {
   std::ifstream istr ("test.txt", std::ios::in);
+  int nblocks = 0;
+  istr >> nblocks;
+  for( int i = 0; i < nblocks; i++) {
+    read_data(istr);
+  }
   return 0;
 }
