d12e293e64b169733f5ed4b885809aaad484db60
Alexander Fedosin <alexander.fedosin@itseez3d.com>
2019-12-10 17:15:09 +0300

    small-updates

--- ch1/solution8.cpp
+++ ch1/solution8.cpp
@@ -3,18 +3,24 @@
 #include <string>
 
 char cand[20][80];
+int votes[1000][3];
 int ncand = 0;
  
 void read_data(std::istream &istr) {
   std::string buf;
-  std::getline(istr, buf); //empty
+  //std::getline(istr, buf); //empty
   istr >> ncand;
-  std::getline(istr, buf); //empty
+  istr.ignore(1);
   for(int i= 0; i < ncand; i++){
     std::getline(istr, buf);
     strcpy(cand[i], buf.c_str());
     std::cout << cand[i] << std::endl;
   }
+  int ctr = 0;
+  while(istr >> votes[ctr][0] >> votes[ctr][1] >> votes[ctr][2]) {
+    std::cout << votes[ctr][0] << votes[ctr][1] << votes[ctr][2];
+  }
+  istr.ignore(1);
   //  std::cout << ncand << std::endl;
   
 }
@@ -23,7 +29,9 @@ int main (void) {
   std::ifstream istr ("test.txt", std::ios::in);
   int nblocks = 0;
   istr >> nblocks;
+  
   for( int i = 0; i < nblocks; i++) {
+    istr.ignore(1); //ignore empty line
     read_data(istr);
   }
   return 0;
