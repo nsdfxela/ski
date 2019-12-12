6fe4124088d920152b8c1c0fc9cf6b2f3305ff64
Alexander Fedosin <alexander.fedosin@itseez3d.com>
2019-12-12 17:27:01 +0300

    fixed-input

--- ch1/solution8.cpp
+++ ch1/solution8.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 
 char cand[20][80];
 int votes[1000][3];
@@ -8,30 +9,28 @@ int ncand = 0;
  
 void read_data(std::istream &istr) {
   std::string buf;
-  //std::getline(istr, buf); //empty
   istr >> ncand;
-  istr.ignore(1);
+  istr.ignore();
+
   for(int i= 0; i < ncand; i++){
     std::getline(istr, buf);
     strcpy(cand[i], buf.c_str());
-    std::cout << cand[i] << std::endl;
+    //    std::cout << cand[i] << std::endl;
   }
   int ctr = 0;
-  while(istr >> votes[ctr][0] >> votes[ctr][1] >> votes[ctr][2]) {
-    std::cout << votes[ctr][0] << votes[ctr][1] << votes[ctr][2];
+  while(getline(istr, buf)) {
+    if(buf == "") break;
+    std::stringstream ss(buf);
+    ss >> votes[ctr][0] >> votes[ctr][1] >> votes[ctr][2];
   }
-  istr.ignore(1);
-  //  std::cout << ncand << std::endl;
-  
 }
 
 int main (void) {
   std::ifstream istr ("test.txt", std::ios::in);
   int nblocks = 0;
   istr >> nblocks;
-  
-  for( int i = 0; i < nblocks; i++) {
-    istr.ignore(1); //ignore empty line
+  istr.ignore(1); //ignore empty line
+  for( int i = 0; i < nblocks; i++) {  
     read_data(istr);
   }
   return 0;
