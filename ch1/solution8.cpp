a1d39994dc973f35e7fc0db2492a076f6a010964
Alexander Fedosin <alexander.fedosin@itseez3d.com>
2019-12-13 16:14:47 +0300

    updates

--- ch1/solution8.cpp
+++ ch1/solution8.cpp
@@ -4,8 +4,9 @@
 #include <sstream>
 
 char cand[20][80];
-int votes[1000][3];
+int votes[1000][20];
 int ncand = 0;
+int nvotes = 0;
  
 void read_data(std::istream &istr) {
   std::string buf;
@@ -17,12 +18,32 @@ void read_data(std::istream &istr) {
     strcpy(cand[i], buf.c_str());
     //    std::cout << cand[i] << std::endl;
   }
-  int ctr = 0;
+  
   while(getline(istr, buf)) {
     if(buf == "") break;
     std::stringstream ss(buf);
-    ss >> votes[ctr][0] >> votes[ctr][1] >> votes[ctr][2];
+    for(int i = 0; i < ncand; i++){
+      ss >> votes[nvotes++][i];
+    }
   }
+}
+void nullall(){
+  ncand = 0;
+  nvotes = 0;
+}
+
+std::string get_winner(int level){
+  int vc[ncand];
+  memset(vc, 0, sizeof vc);
+  for(int i = 0; i < nvotes; i++) {
+      vc[votes[i][level]-1]++;
+  }
+
+  for(int i =0;i < ncand;i++){
+    std::cout << vc[i] << " ";
+  }
+  std::cout << std::endl;
+  return "";
 }
 
 int main (void) {
@@ -32,6 +53,8 @@ int main (void) {
   istr.ignore(1); //ignore empty line
   for( int i = 0; i < nblocks; i++) {  
     read_data(istr);
+    std::cout << get_winner(0);
+    nullall();
   }
   return 0;
 }
