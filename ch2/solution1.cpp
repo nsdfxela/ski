a038c32bc808cd1fe86f8668811396482522ae06
Alexander Fedosin <alexander.fedosin@itseez3d.com>
2019-12-18 12:34:20 +0300

    ch2-ex1

--- ch2/solution1.cpp
+++ ch2/solution1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <set>
 
 int numbers[3000];
 int n;
@@ -14,23 +15,30 @@ bool read_data(std::istream &istr)
     }
     return true;
 }
-
+std::set<int> s;
+#define NJ "Not jolly"
+#define J "Jolly"
 std::string solve() {
     int p = -1;
     for (int i = 0; i < n-1; i++) {
         int m = abs (numbers[i + 1] - numbers[i]);
-        if (i && abs(m - p) != 1) {
+        /*if (i && abs(m - p) != 1) {
             return "Not jolly";
-        }
-        if (!i && m != 1) {
-            return "Not jolly";
-        }
+        }*/
+        s.insert(m);
         p = m;
     }
-    return "Jolly";
+
+    for (int i = 1; i <= n-1; i++) {
+        if (s.find(i) == s.end()) {
+            return NJ;
+        }
+    }
+    return J;
 }
 
 int main(void) {
+    //std::istream &istr = std::cin;
     std::ifstream istr(R"(D:\study\ski\ch2\test.txt)", std::ios::in);
     while (read_data(istr)) {
         std::cout << solve() << std::endl;
