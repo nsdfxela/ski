147a46f679fb9087616675c5be94f78bb84dda9d
Alexander Fedosin <alexander.fedosin@itseez3d.com>
2020-01-13 15:46:12 +0300

    updates

--- ch2/solution4.cpp
+++ ch2/solution4.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <fstream>
+#include <map>
+#include <string>
+#include <sstream>
 
 int main(void) {
 
@@ -8,6 +11,28 @@ int main(void) {
 #else
     std::ifstream istr(R"(D:\study\ski\ch2\test.txt)", std::ios::in);
 #endif
-
+    int wc;
+    istr >> wc;
+    std::string dict[1000];
+    std::map<char, int> lc;
+    for (int i = 0; i < wc; i++) {
+        istr >> dict[i];
+        for (int j = 0; j < dict[i].length(); j++) {
+            lc[dict[i][j]] ++;
+        }
+    }
+    istr.ignore(1);
+    while (istr) {
+        std::string buf;
+        std::getline(istr, buf);
+        std::stringstream ss(buf);
+        while (ss) {
+            std::string w;
+            ss >> w;
+            std::cout << w << " ";
+        }
+        std::cout << '\n';
+        
+    }
     return 0;
 }
