34bb76c463efd0aa4edc06f8c525bf9b9af0561f
Alexander Fedosin <alexander.fedosin@itseez3d.com>
2020-01-14 15:53:58 +0300

    updates

--- ch2/solution4.cpp
+++ ch2/solution4.cpp
@@ -3,6 +3,42 @@
 #include <map>
 #include <string>
 #include <sstream>
+#include <vector>
+#include <algorithm>
+
+struct wrd 
+{
+    std::string str;
+    std::string fp;
+    std::map<char, int> d;
+    wrd(const std::string &s) : str{ s } {
+        for (int i = 0; i < s.length(); i++) {
+            d[s[i]]++;
+        }
+        std::vector<int> frq;
+        std::transform(d.begin(), d.end(), std::back_inserter(frq), [](std::pair<char, int> p) -> int { return p.second; });
+        std::sort(frq.begin(), frq.end());
+        fp.resize(frq.size());
+        std::ostringstream os;
+        for (int i = 0; i < frq.size(); i++) {
+            os << frq[i];
+        }
+        fp = os.str();
+    }
+    
+};
+
+std::map<std::string, std::vector<wrd>> lc;
+
+
+std::string solve(const std::vector<std::string> &src) {
+   
+    wrd w(src[0]);
+    for (int i = 0; i < src.size(); i++) {
+        
+    }
+    return "";
+}
 
 int main(void) {
 
@@ -14,23 +50,24 @@ int main(void) {
     int wc;
     istr >> wc;
     std::string dict[1000];
-    std::map<char, int> lc;
     for (int i = 0; i < wc; i++) {
         istr >> dict[i];
-        for (int j = 0; j < dict[i].length(); j++) {
-            lc[dict[i][j]] ++;
-        }
+        wrd w(dict[i]);
+        lc[w.fp].push_back(w);
     }
     istr.ignore(1);
+
     while (istr) {
         std::string buf;
         std::getline(istr, buf);
         std::stringstream ss(buf);
+        std::vector<std::string> vec;
         while (ss) {
             std::string w;
             ss >> w;
-            std::cout << w << " ";
+            vec.push_back(w);
         }
+        std::cout << solve(vec);
         std::cout << '\n';
         
     }
