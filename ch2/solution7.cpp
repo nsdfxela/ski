80ae475ea1189ea97c00861fc31bae861ad1dd19
nsdfxela <nsdfxela@gmail.com>
2020-03-09 13:21:35 +0100

    updates

--- ch2/solution7.cpp
+++ ch2/solution7.cpp
@@ -1,19 +1,35 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-
+#include <map>
+#include <sstream>
 
 struct entry {
-	int participant, exercise, time, L;
+	/*entry(int part, std::istream &istr) : participant{ part } {
+		istr >> solved >> time;
+	}*/
+	int participant, solved, time;	
 };
+std::map<int, entry> entries;
+
 void read_block(std::istream &istr) {
 	
 	std::string buf;
 	while (true) {
 		std::getline(istr, buf);
 		if (buf.empty()) { break; }
-		
-		//istr >> participant >> exercise >> time >> L;
+		std::stringstream ss(buf);
+		int participant, solved, time;
+		char L;
+		ss >> participant >> solved >> time >> L;
+		if (L == 'C') {
+			entries[participant].solved++;
+			entries[participant].time += time;
+		}
+		else if (L == 'I') {
+			entries[participant].time += 20;
+		}
+		//>> exercise >> time >> L;
 
 		//std::cout << buf << std::endl;
 	}
@@ -23,7 +39,7 @@ int main(void) {
 #ifdef __GNUC__
 	std::istream &istr = std::cin;
 #else
-	std::fstream istr(R"(D:\repos\ski\ch2\test.txt)");
+	std::fstream istr(R"(C:\FUNHOUSE\Skiena-20191022\solutions\ch2\test.txt)");
 #endif
 	int nblocks;
 	istr >> nblocks;
