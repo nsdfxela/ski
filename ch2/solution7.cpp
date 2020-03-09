bd618a32aadcf5f41cba8d24af39e0bdb962aacb
Alex Fedosin <nsdxela@gmail.com>
2020-03-09 10:58:27 +0300

    read-implemented

--- ch2/solution7.cpp
+++ ch2/solution7.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include <string>
 #include <fstream>
+
+void read_block(std::istream &istr) {
+	
+	std::string buf;
+	while (true) {
+		std::getline(istr, buf);
+		if (buf.empty()) { break; }
+		//std::cout << buf << std::endl;
+	}
+}
+
 int main(void) {
 #ifdef __GNUC__
 	std::istream &istr = std::cin;
@@ -8,7 +20,11 @@ int main(void) {
 #endif
 	int nblocks;
 	istr >> nblocks;
-	std::cout << nblocks;
+
+	istr.ignore(2);
+	for (int i = 0; i < nblocks; i++) {
+		read_block(istr);
+	}
 
 	return 0;
 }
