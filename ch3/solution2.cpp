336448e4ecf5be34d91f63c502daffc3c7d7a951
nsdfxela <nsdfxela@gmail.com>
2020-05-01 12:23:41 +0300

    search-method

--- ch3/solution2.cpp
+++ ch3/solution2.cpp
@@ -1,19 +1,44 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
+#include <vector>
 
-std::string hor[50];
-std::string rhor[50];
-std::string vert[50];
-std::string rvert[50];
 
-std::string mdiag[50];
-std::string rmdiag[50];
+std::string hor[50]; 
 
-std::string sdiag[50];
-std::string rsdiag[50];
+std::string rhor[50]; 
+std::string vert[50]; 
+std::string rvert[50];  
+
+struct point { int r; int c; };
+
+std::string mdiag[50]; point _mdiag[50][20];
+std::string rmdiag[50]; point _rmdiag[50][20];
+
+std::string sdiag[50]; point _sdiag[50][20];
+std::string rsdiag[50]; point _rsmdiag[50][20];
 
 #define N 50
+
+std::vector<point> search(const std::string& str) {
+    std::vector<point> res;
+    size_t pos;
+    for (int i = 0; i < 50; i++) {
+        pos = hor[i].find(str); 
+        if (pos != std::string::npos) { res.push_back(point{ i, (int)pos }); }
+        pos = rhor[i].find(str);
+        if (pos != std::string::npos) { res.push_back(point{ i, (int)(rhor->size() - pos) }); }
+        pos = vert[i].find(str);
+        if (pos != std::string::npos) { res.push_back(point{ (int)pos, i }); }
+        pos = rvert[i].find(str);
+        if (pos != std::string::npos) { res.push_back(point{ (int)(rvert[i].size() - pos), i }); }
+        pos = mdiag[i].find(str);
+        if(pos != std::string::npos) { res.push_back( _mdiag[i][pos] ); }
+    }
+    return res;
+}
+
 void form(std::string grid[50], int R, int C ) {
     for (int i = 0; i < N; i++) {
         rhor[i].resize(C);
@@ -28,29 +53,61 @@ void form(std::string grid[50], int R, int C ) {
         }
         std::copy(vert[i].rbegin(), vert[i].rend(), rvert[i].begin());
     }
-
     int Mn = R < C ? R : C;
-
     int mdc = 0;
     for (int offset = C-1; offset >=0; offset--) {
+        std::stringstream ss;
         int cc = 0;
-        mdiag[mdc].resize(Mn);
         for (int i = 0, j = 0; (i < R) && (j + offset < C); i++, j++) {
-            mdiag[mdc][cc++] = grid[i][j+offset];
+            ss << grid[i][j+offset];
+            
         }
+        mdiag[mdc].assign(ss.str());
         mdc++;
         
     }
     for (int offset = 1; offset < R; offset++) {
         int cc = 0;
-        mdiag[mdc].resize(Mn);
+        std::stringstream ss;
         for (int i = 0, j = 0; (i < R) && (j + offset < R); i++, j++) {
-            mdiag[mdc][cc++] = grid[i + offset][j];
+            ss << grid[i + offset][j];
+            point p{ i + offset , j};
+            _mdiag[mdc][cc++] = p;
         }
+        mdiag[mdc].assign(ss.str());
         mdc++;
     }
-}
 
+    for (int i = 0; i < mdc; i++) {
+        rmdiag[i].resize(mdiag[i].size());
+        std::copy(mdiag[i].rbegin(), mdiag[i].rend(), rmdiag[i].begin());
+    }
+    mdc = 0;
+    for (int offset = 0; offset < R; offset++) {
+        std::stringstream ss;
+        int cc = 0;
+        for (int i = 0, j = C-1; (i + offset < R) && (j >= 0); i++, j--) {
+            ss << grid[i + offset][j];
+            point p{ i + offset , j };
+            _mdiag[mdc][cc++] = p;
+        }
+        sdiag[mdc].assign(ss.str());
+        mdc++;
+    }
+    for (int offset = 1; offset < C; offset++) {
+        std::stringstream ss;
+        for (int i = 0, j = C - 1; (i < R) && (j - offset >= 0); i++, j--) {
+            ss << grid[i][j - offset];
+        }
+        sdiag[mdc].assign(ss.str());
+        mdc++;
+    }
+    for (int i = 0; i < mdc; i++) {
+        rsdiag[i].resize(sdiag[i].size());
+        std::copy(sdiag[i].rbegin(), sdiag[i].rend(), rsdiag[i].begin());
+    }
+
+}
 int main(void) {
 #if __GNUC__
     std::istream& istr = std::cin;
@@ -70,19 +127,16 @@ int main(void) {
             std::getline(istr, hor[i]);
         }
         form(hor, m, n);
+        
         int k;
         istr >> k;
         std::string words[20];
         istr.ignore(1);
         for (int i = 0; i < k; i++) {
             std::getline(istr, words[i]);
+            std::vector<point> found = search(words[i]);
         }
     }
-    
-
-    
-
-
     
     return 0;
 }
