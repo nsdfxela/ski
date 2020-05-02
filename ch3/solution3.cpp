5c46ae59ff53a020209f50de1c12345ae00b41e7
nsdfxela <nsdfxela@gmail.com>
2020-05-02 16:31:24 +0300

    initial-stuff-ex3

---
+++ ch3/solution3.cpp
@@ -0,0 +1,15 @@
+#include <fstream>
+#include <string>
+#include <iostream>
+
+int main(void) {
+
+#if __GNUC__
+    std::istream& istr = std::cin;
+#else
+    std::ifstream istr(R"(C:\fedosin\repos\ski\ch3\test.txt)");
+#endif
+
+
+    return 0;
+}
