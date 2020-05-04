8f0526a6283cf333fe1d60c3a60c02b32c66aebc
nsdfxela <nsdfxela@gmail.com>
2020-05-04 17:22:54 +0300

    first-sent-sol

--- ch3/solution4.cpp
+++ ch3/solution4.cpp
@@ -4,42 +4,53 @@
 #include <vector>
 #include <map>
 #include <algorithm>
+#include <sstream>
+
 
 const std::string KEY = "the quick brown fox jumps over the lazy dog";
-std::map<char, int> KEY_FP;
+std::string KEYfreq;
+std::string alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-std::map<char, int> getFingerPrint(const std::string &str) {
-    std::map<char, int> fp;
-    for (int j = 0; j < str.size(); j++) {
-        fp[str[j]]++;
-    }
-    return fp;
-}
+std::string calcFreq(const std::string &origin) {
+    int aiter=0;
+    std::map<char, char> mp;
+    std::ostringstream ss;
 
-bool equal(const std::map<char, int> &a, const std::map<char, int> &b) {
-    if (a.size() != b.size()) {
-        return false;
+    for (int i = 0; i < origin.size(); i++) {
+        if (origin[i] == ' ') {
+            ss << ' ';
+        } else if (mp.find(origin[i]) == mp.end()) {
+            mp[origin[i]] = alphabet[aiter];
+            ss << alphabet[aiter++];
+        } else {
+            ss << mp[origin[i]];
+        }
     }
-    std::vector<int> avals, bvals;
-    for (auto it : a) {
-        avals.push_back(it.second);
-    }
-    for (auto it : b) {
-        bvals.push_back(it.second);
-    }
-    std::sort(avals.begin(), avals.end());
-    std::sort(bvals.begin(), bvals.end());
-    return std::equal(avals.begin(), avals.end(), bvals.begin());
+    return ss.str();
 }
 
 void solve(std::vector<std::string>& tc) {
-    std::vector< std::map<char, int>> fps;
-    std::vector<bool> potentialKeys;
+    std::map<char, char> dct;
     for (int i = 0; i < tc.size(); i++) {
-        auto fp = getFingerPrint(tc[i]);
-        fps.push_back(fp);
-        potentialKeys.push_back(equal(fp, KEY_FP));
+        std::string fp = calcFreq(tc[i]);
+        if (fp == KEYfreq) {
+            for (int j = 0; j < KEY.size(); j++) {
+                dct[tc[i][j]] = KEY[j];
+    
+            }
+            break;
+        }
     }
+    if (dct.size() != 27) {
+        std::cout << "No solution." << std::endl;
+    }
+    for (int i = 0; i < tc.size(); i++) {
+        if (i) std::cout << std::endl;
+        for (int j = 0; j < tc[i].size(); j++) {
+            std::cout << dct[tc[i][j]];
+        }
+    }
+
 }
 
 int main(void) {
@@ -48,7 +59,7 @@ int main(void) {
 #else 
     std::ifstream istr(R"(C:\fedosin\repos\ski\ch3\test.txt)");
 #endif
-    KEY_FP = getFingerPrint(KEY);
+    KEYfreq = calcFreq(KEY);
     int tc;
     istr >> tc;
     istr.ignore(2);
@@ -62,6 +73,8 @@ int main(void) {
                 testcase.push_back(buffer);
             }
         } while (!buffer.empty() && istr.good());
+        if (i) 
+            std::cout << std::endl << std::endl;
         solve(testcase);
     }
 
