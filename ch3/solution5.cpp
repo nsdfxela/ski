2dd5fca3fd35d48c1125f3f3764635fdb48e9dd2
nsdfxela <nsdfxela@gmail.com>
2020-05-04 17:56:50 +0300

    data-reading

--- ch3/solution5.cpp
+++ ch3/solution5.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
 
 int main(void) {
 
@@ -10,6 +13,29 @@ int main(void) {
     std::ifstream istr(R"(C:\fedosin\repos\ski\ch3\test.txt)");
 #endif
 
+    std::string buffer;
+    while (true) {
+        std::vector<std::string> sol;
+        std::vector<std::string> ans;
+        
+        std::getline(istr, buffer);
+        if (buffer == "0") break;
+        std::stringstream ss(buffer);
+        int c1;
+        ss >> c1;
+        ss.clear();
+        for (int i = 0; i < c1; i++) {
+            std::getline(istr, buffer);
+            sol.push_back(buffer);
+        }
+        int c2;
+        istr >> c2;
+        istr.ignore();
+        for (int i = 0; i < c2; i++) {
+            std::getline(istr, buffer);
+            ans.push_back(buffer);
+        }
+    }
 
     return 0;
 }
