c92eff40bf25bc13b64cd72660c302b9a6fb13cc
nsdfxela <nsdfxela@gmail.com>
2020-05-05 09:27:37 +0300

    dumbest-accept-ever-ch3-ex5

--- ch3/solution5.cpp
+++ ch3/solution5.cpp
@@ -4,6 +4,65 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <set>
+
+std::set<char> digits{ '0', '1', '2', '3', '4', '5', '6', '7','8','9' };
+
+
+int checkPe(std::string &s, std::string& a) {
+    std::vector<int> vs, va;
+    for (int j = 0; j < s.size(); j++) {    
+        if (digits.find(s[j]) != digits.end()) {
+            vs.push_back((int)s[j] - 48);
+        }
+    }
+
+    for (int j = 0; j < a.size(); j++) {
+        if (digits.find(a[j]) != digits.end()) {
+            va.push_back((int)a[j] - 48);
+        }
+    }
+
+    if (vs.size() == va.size() && std::equal(vs.begin(), vs.end(), va.begin())) {
+        return 1;
+    }
+    else {
+        return 2;
+    }
+}
+int solve(std::vector<std::string>& s, std::vector<std::string>& a) {
+    int result = 0;
+    //0 accepted 1 presentation 2 error
+    if (a.size() != s.size()) {
+        std::stringstream ssa, sss;
+        for (int i = 0; i < a.size(); i++) {
+            ssa << a[i];
+        }
+        for (int i = 0; i < s.size(); i++) {
+            sss << s[i];
+        }
+        std::string ssas = ssa.str();
+        std::string ssss = sss.str();
+        return checkPe(ssas, ssss);
+    }
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] != a[i]) {
+            std::stringstream ssa, sss;
+            for (int _i = 0; _i < a.size(); _i++) {
+                ssa << a[_i];
+            }
+            for (int _i = 0; _i < s.size(); _i++) {
+                sss << s[_i];
+            }
+            std::string ssas = ssa.str();
+            std::string ssss = sss.str();
+
+            return checkPe(ssas, ssss);
+        }
+        
+    }
+    return result;
+}
 
 int main(void) {
 
@@ -14,12 +73,13 @@ int main(void) {
 #endif
 
     std::string buffer;
+    int run = 0;
     while (true) {
         std::vector<std::string> sol;
         std::vector<std::string> ans;
         
         std::getline(istr, buffer);
-        if (buffer == "0") break;
+        if (!istr.good() || buffer == "0") break;
         std::stringstream ss(buffer);
         int c1;
         ss >> c1;
@@ -35,6 +95,20 @@ int main(void) {
             std::getline(istr, buffer);
             ans.push_back(buffer);
         }
+        int result = solve(sol, ans);
+        std::cout << "Run #" << ++run << ": ";
+        switch (result) {
+        case 0:
+            std::cout << "Accepted";
+            break;
+        case 1:
+            std::cout << "Presentation Error";
+            break;
+        case 2:
+            std::cout << "Wrong Answer";
+            break;
+        }
+        std::cout << std::endl;
     }
 
     return 0;
