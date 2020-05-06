2de630596ff3c05d9f61f7a7fe582439cc12b29f
nsdfxela <nsdfxela@gmail.com>
2020-05-06 14:47:36 +0300

    reading-done

--- ch3/solution6.cpp
+++ ch3/solution6.cpp
@@ -11,6 +11,26 @@ int main(void) {
 #else
     std::ifstream istr(R"(C:\fedosin\repos\ski\ch3\test.txt)");
 #endif
+    int tb;
+    istr >> tb;
+    istr.ignore(2);
+    for (int i = 0; i < tb; i++) {
+        std::vector<int> files[144];
+        std::string buffer;
+        int c = 0;
+        while (1) {
+            std::getline(istr, buffer);
+            if (!istr.good() || buffer.empty()) {
+                break;
+            }
+            for (int j = 0; j < buffer.size(); j++) {
+                if (buffer[j] == '1') files[c].push_back(1);
+                else if (buffer[j] == '0') files[c].push_back(0);
+                else break;
+            }
+            c++;
 
+        }
+    }
     return 0;
 }
