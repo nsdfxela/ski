8f0ebeb7d72b65a7b85395235bc2a6d299ec2aac
nsdfxela <nsdfxela@gmail.com>
2020-05-07 15:40:36 +0300

    search-recusive-stuff

--- ch3/solution6.cpp
+++ ch3/solution6.cpp
@@ -3,22 +3,81 @@
 #include <vector>
 #include <fstream>
 #include <map>
+#include <set>
+
+bool check(const std::string& var, const std::string &str, std::map<std::string, int> visited) {
+    if (var.find(str) != 0) {
+        return false;
+    } else {
+        auto &it = visited.find(str);
+        if (it == visited.end() || !it->second) {
+            return false;
+        } else {
+            it->second --;
+            //err here
+
+            std::string complement = var.substr(var.rfind(str), std::string::npos);
+            auto& it2 = visited.find(complement);
+            if (it2 != visited.end() && it2->second > 0) {
+                it2->second--;
+                bool allchecked = true;
+                for (auto& it3 = visited.begin(); it3 != visited.end(); it3++) {
+                    if (it3->second) {
+                        allchecked = false;
+                        bool good = check(var, it3->first, visited);
+                        if (good) {
+                            std::cout << it->first << " " << it->second << std::endl;
+                            return good;
+                        }
+                    }
+                }
+                
+                return allchecked;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+}
 
 void solve(std::string files[144], int c) {
     int totalLetters = 0;
     int letters = 0;
     int linksNeed = c / 2;
     std::map<int, std::vector<int>> mp;
-    int bitmap[144];
-    memset(bitmap, 0, sizeof(int) * 144);
+    std::vector<bool> bm(c);
     for (int i = 0; i < c; i++) {
-        mp[files[i].size()].push_back(c);
+        mp[files[i].size()].push_back(i);
         totalLetters += files[i].size();
     }
     letters = totalLetters / c * 2;
     std::map<int, int> mtch;
+    std::set<std::string> vars;
+
     for (int i = 0; i < c; i++) {
+        auto matches = mp.find(letters - files[i].size());
+        for (int j = 0; j < matches->second.size(); j++) {
+            std::string var = files[i] + files[matches->second[j]];
+            vars.insert(var);
+        }
     }
+    std::map<std::string, int> visited;
+    for (int i = 0; i < c; i++) {
+        visited[files[i]]++;
+    }
+    bool res = false;
+    for (auto it = vars.begin(); it != vars.end(); it++) {
+        for (int i = 0; i < c; i++)
+        {
+            res = check(*it, files[i], visited);
+            if (res) {
+                std::cout << *it << std::endl;
+                return;
+            }
+        }
+    }
+
 }
 
 int main(void) {
@@ -31,6 +90,7 @@ int main(void) {
     istr >> tb;
     istr.ignore(2);
     for (int i = 0; i < tb; i++) {
+        if (i) std::cout << std::endl;
         std::string files[144];
         std::string buffer;
         int c = 0;
