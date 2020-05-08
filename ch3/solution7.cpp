0817a28a5997f3c2761460290b6e80af0aa823fd
nsdfxela <nsdfxela@gmail.com>
2020-05-08 16:05:57 +0300

    data-read

--- ch3/solution7.cpp
+++ ch3/solution7.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <set>
+
+std::set<std::string> dict;
+void solve(const std::string& str1, const std::string& str2) {
+
+
+}
 
 int main(void) {
 
@@ -9,6 +16,21 @@ int main(void) {
 #else
     std::ifstream istr(R"(C:\fedosin\repos\ski\ch3\test.txt)");
 #endif
+    while (1) {
+        std::string buf;
+        std::getline(istr, buf);
+        if (!istr.good() || buf.empty()) {
+            break;
+        }
+        dict.insert(buf);
+    }
+    std::string str1, str2;
+    while (istr.good()) {
+        istr >> str1 >> str2;
+        if (istr.good()) {
+            solve(str1, str2);
+        }
+    }
 
     return 0;
 }
