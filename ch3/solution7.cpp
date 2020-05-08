84ace26e2274dd324b436145e7be82b8888a28a4
nsdfxela <nsdfxela@gmail.com>
2020-05-08 17:33:51 +0300

    distance-calc

--- ch3/solution7.cpp
+++ ch3/solution7.cpp
@@ -1,12 +1,43 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <set>
+#include <vector>
 
-std::set<std::string> dict;
+int distance(const std::string& str1, const std::string& str2) {
+    int sz = str1.size() < str2.size() ? str1.size() : str2.size();
+    int res = 0;
+    for (int i = 0; i < sz; i++) {
+        if (str1[i] != str2[i]) {
+            res++;
+        } 
+        if (res > 1) {
+            res = 100;
+        }
+    }
+    return res;
+}
+std::vector<std::string> dict;
 void solve(const std::string& str1, const std::string& str2) {
+    int ds = dict.size();
+    int** tbl;
+    tbl = new int* [ds];
+    for (int i = 0; i < ds; i++) {
+        tbl[i] = new int[ds];
+        memset(tbl[i], 0, ds * sizeof(int));
+    }
+    for (int i = 0; i < ds; i++) {
+        for (int j = 0; j < ds; j++) {
+            tbl[i][j] = distance(dict[i], dict[j]);
+            std::cout << tbl[i][j] << "   ";
+        }
 
+        std::cout << std::endl;
+    }
 
+    for (int i = 0; i < ds; i++) {
+        delete [] tbl[i];
+    }
+    delete[] tbl;
 }
 
 int main(void) {
@@ -22,7 +53,7 @@ int main(void) {
         if (!istr.good() || buf.empty()) {
             break;
         }
-        dict.insert(buf);
+        dict.push_back(buf);
     }
     std::string str1, str2;
     while (istr.good()) {
