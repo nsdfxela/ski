878ecffbae84317b4edb037bcdff0602634657de
nsdfxela <nsdfxela@gmail.com>
2020-05-12 09:08:25 +0300

    trying-to-solve

--- ch3/solution8.cpp
+++ ch3/solution8.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
+#include <sstream>
+
+void solve(const std::vector<std::string>& data) {
+    for (int i = 0; i < data.size(); i++) {
+        std::istringstream sstr(data[i]);
+        std::string buffer;
+        std::getline(sstr, buffer, ' ');
+
+    }
+}
 
 int main(void){
 
@@ -9,6 +20,15 @@ int main(void){
 #else
     std::ifstream istr(R"(C:\fedosin\repos\ski\ch3\test.txt)");
 #endif
-
+    std::vector<std::string> data;
+    while (1) {
+        std::string buffer;
+        std::getline(istr, buffer);
+        if (!istr.good()) {
+            break;
+        }
+        data.push_back(buffer);
+        solve(data);
+    }
     return 0;
 }
