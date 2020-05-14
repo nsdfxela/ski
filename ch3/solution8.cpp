b785b803c2708e44bd4fdc10ad15b3fe1e2c18a6
nsdfxela <nsdfxela@gmail.com>
2020-05-14 15:23:35 +0300

    shittyshit

--- ch3/solution8.cpp
+++ ch3/solution8.cpp
@@ -7,60 +7,25 @@
 std::ostringstream oss;
 
 std::string trim(const std::string& str, int tr = 1) {
-    if (!tr) {
-        return str;
+    std::string res = str;
+    int idx = res.size()-1;
+    while (idx >= 0 && res[idx] == ' ') {
+        idx--;
     }
-    int idx = 0;
-    while (idx < str.size() && str[idx] == ' ') {
+    res = res.substr(0, idx+1);
+
+    if (!tr) {
+        return res;
+    }
+    idx = 0;
+    while (idx < res.size() && res[idx] == ' ') {
         idx++;
     }
-    return str.substr(idx);
-}
-void append(std::ostringstream &str, int tr = 1) {
-    if (trim(oss.str(), tr).size() + str.str().size() >= 72) {
-        std::cout << trim(oss.str(), tr) << std::endl;
-        oss.swap(std::ostringstream());
-    }
-    oss << str.str();
-    str.swap(std::ostringstream());
-}
-
-void append(char c, int tr = 1) {
-    if (trim(oss.str(), tr).size() + 1 >= 72) {
-        std::cout << trim(oss.str(), tr) << std::endl;
-        oss.swap(std::ostringstream());
-    }
-    oss << c;
+    return res.substr(idx);
 }
 
 void solve(const std::vector<std::string>& data, const std::vector<int> canDeleteEndl) {
-    int prevLineCanDelete = 0;
-    for (int i = 0; i < data.size(); i++) {
-        std::ostringstream word;
-        for (int j = 0; j < data[i].size(); j++) {
-            if (data[i][j] == ' ' ) {
-                if (word.str().size()) {
-                    append(word, prevLineCanDelete);
-                }
-                append(' ', prevLineCanDelete);
-            } else {
-                word << data[i][j];
-            }
-        }
-        if (canDeleteEndl[i]) {
-            if (word.str().size()) {
-                append(word, prevLineCanDelete);
-                append(' ', prevLineCanDelete);
-            }
-        } else {
-            if (word.str().size()) {
-                append(word, prevLineCanDelete);
-            }
-            std::cout << oss.str() << std::endl;
-            oss.swap(std::ostringstream());
-        }
-        prevLineCanDelete = canDeleteEndl[i];
-    }    
+
 }
 
 void checkEndl(const std::vector<std::string>& data, std::vector<int>& canDeleteEndl) {
