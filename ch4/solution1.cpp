e35edb46f04b2374e1296b210140bf74ddace2a6
nsdfxela <nsdfxela@gmail.com>
2020-05-16 12:55:32 +0300

    read-data

--- ch4/solution1.cpp
+++ ch4/solution1.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <sstream>
+#include <vector>
+
+
+void solve(std::vector<int>& v) {
+
+
+}
 
 int main(void) {
 
@@ -9,7 +17,25 @@ int main(void) {
 #else
     std::ifstream istr(R"(C:\fedosin\repos\ski\ch4\test.txt)");
 #endif
-
+    int tc;
+    istr >> tc;
+    istr.ignore();
+    for (int i = 0; i < tc; i++) {
+        std::string buf;
+        std::getline(istr, buf);
+        std::stringstream ss(buf);
+        std::vector<int> tcase;
+        while (1) {
+            int val;
+            ss >> val;
+            tcase.push_back(val);
+            if (!ss.good()) {
+                break;
+            }
+        }
+        solve(tcase);
+        
+    }
 
     return 0;
 }
