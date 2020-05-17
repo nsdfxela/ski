fe84e5784a1c5de8c22ad80600ff4809fcd6a2a9
nsdfxela <nsdfxela@gmail.com>
2020-05-17 16:04:59 +0300

    initial-sol2

---
+++ ch4/solution2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(void) {
+
+#if __GNUC__
+    std::istream& istr = std::cin;
+#else
+    std::ifstream istr(R"(C:\fedosin\repos\ski\ch4\test.txt)");
+#endif
+
+
+    return 0;
+}
