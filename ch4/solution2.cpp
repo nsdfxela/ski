bc85f9209a889bc3a173a6fd901bd1ed4a17ce46
nsdfxela <nsdfxela@gmail.com>
2020-05-18 13:57:25 +0300

    read-data-for-sol2

--- ch4/solution2.cpp
+++ ch4/solution2.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <sstream>
+
+void solve(std::vector<int> &stk) {
+
+}
 
 int main(void) {
 
@@ -9,7 +15,24 @@ int main(void) {
 #else
     std::ifstream istr(R"(C:\fedosin\repos\ski\ch4\test.txt)");
 #endif
+    std::string buffer;
+    while (1) {
 
+        if (!istr.good()) {
+            break;
+        }
+        std::getline(istr, buffer);
+        if (!buffer.empty()) {
+            std::stringstream ss(buffer);
+            std::vector<int> s;
+            while (ss.good()) {
+                int b;
+                ss >> b;
+                s.push_back(b);
+            }
+            solve(s);
+        } 
+    }
 
     return 0;
 }
