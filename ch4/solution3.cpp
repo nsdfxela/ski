36235eddf5c8eef411189dcd4b4d6fe35b4c4ef0
nsdfxela <nsdfxela@gmail.com>
2020-05-22 11:46:42 +0300

    data-read

---
+++ ch4/solution3.cpp
@@ -0,0 +1,40 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+
+void solve(std::vector<int> &vec) {
+
+
+}
+
+
+int main(void) {
+
+#if __GNUC__
+    std::istream& istr = std::cin;
+#else
+    std::ifstream istr(R"(C:\fedosin\repos\ski\ch4\test.txt)");
+#endif
+    int tc;
+    istr >> tc;
+    istr.ignore();
+
+    for (int i = 0; i < tc; i++) {
+        std::vector<int> vec;
+        int n;
+        istr >> n;
+        vec.reserve(n);
+        for (int j = 0; j < n; j++) {
+            int m;
+            istr >> m;
+            vec.push_back(m);
+        }
+        solve(vec);
+        istr.ignore();
+    }
+
+
+    return 0;
+}
