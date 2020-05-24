4e566372360b3c8693156a262d10a7bf10d6085d
nsdfxela <nsdfxela@gmail.com>
2020-05-24 08:24:09 +0300

    read

--- ch4/solution4.cpp
+++ ch4/solution4.cpp
@@ -3,14 +3,35 @@
 #include <iostream>
 #include <vector>
 
+struct hour {
+    int h;
+    int m;
+};
+struct app {
+    hour start;
+    hour end;
+    std::string val;
+};
 
 int main(void) {
 
 #if __GNUC__
     std::istream& istr = std::cin;
 #else 
-    std::ifstream istr (R"("C:\fedosin\repos\ski\ch4\test.txt")");
+    std::ifstream istr (R"(C:\fedosin\repos\ski\ch4\test.txt)");
 #endif
-
+    while (1) {
+        int napp;
+        istr >> napp;
+            if (!istr.good()) {
+            break;
+        }
+        istr.ignore();
+        std::string buffer;
+        for (int i = 0; i < napp; i++) {
+            std::getline(istr, buffer);
+        }
+       
+    }
     return 0;
 }
