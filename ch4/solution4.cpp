2f0af87ecf7c88055c5e1363f57aeb33f1e7a5a1
nsdfxela <nsdfxela@gmail.com>
2020-05-24 08:55:36 +0300

    read-implemented

--- ch4/solution4.cpp
+++ ch4/solution4.cpp
@@ -2,16 +2,33 @@
 #include <string>
 #include <iostream>
 #include <vector>
+#include <sstream>
+
 
 struct hour {
+    static hour parse(const std::string& str) {
+        hour res;
+        std::stringstream ss(str);
+        ss >> res.h;
+        ss.ignore();
+        ss >> res.m;
+        return res;
+    }
     int h;
     int m;
 };
+
 struct app {
     hour start;
     hour end;
     std::string val;
 };
+
+typedef std::vector<app> day;
+
+void solve(day &d) {
+
+}
 
 int main(void) {
 
@@ -28,10 +45,19 @@ int main(void) {
         }
         istr.ignore();
         std::string buffer;
+        day apps;
         for (int i = 0; i < napp; i++) {
             std::getline(istr, buffer);
+            std::string stime = buffer.substr(0, 5);
+            std::string etime = buffer.substr(6, 5);
+            std::string app_str = buffer.substr(12);
+            app a;
+            a.start = hour::parse(stime);
+            a.end = hour::parse(etime);
+            a.val = app_str;
+            apps.push_back(a);
         }
-       
+        solve(apps);
     }
     return 0;
 }
