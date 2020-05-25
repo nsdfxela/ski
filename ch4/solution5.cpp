59ec51b5fe83946f54ba3e0d32f81408bc475600
nsdfxela <nsdfxela@gmail.com>
2020-05-25 15:34:39 +0300

    read-data

--- ch4/solution5.cpp
+++ ch4/solution5.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+
+
+struct ord {
+    int days;
+    int fine;
+};
+
+typedef std::vector<ord> ords;
+
+void solve(ords& O) {
+
+}
 
 int main(void) {
 
@@ -8,7 +21,23 @@ int main(void) {
 #else
     std::ifstream istr(R"(C:\fedosin\repos\ski\ch4\test.txt)");
 #endif
+    int tc;
+    istr >> tc;
+    istr.ignore();
+    for (int i = 0; i < tc; i++) {
+        int nords;
+        istr >> nords;
+        ords o;
 
+        for (int j = 0; j < nords; j++) {
+            ord c;
+            istr >> c.days;
+            istr >> c.fine;
+            o.push_back(c);
+        }
+        solve(o);
+
+    }
 
     return 0;
 }
