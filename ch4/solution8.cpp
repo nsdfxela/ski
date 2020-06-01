d46fb788873202c6b0965b30af37443f87dda2b9
nsdfxela <nsdfxela@gmail.com>
2020-06-01 07:37:48 +0300

    sol8-initial

---
+++ ch4/solution8.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+
+int main(void) {
+#if __GNUC__
+    std::istream& istr = std::cin;
+#else
+    std::ifstream istr(R"(C:\fedosin\repos\ski\ch4\test.txt)");
+#endif
+
+    return 0;
+}
