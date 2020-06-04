4ca5fa3ccd590a6f5be56f3cf47d6f187a5a15aa
nsdfxela <nsdfxela@gmail.com>
2020-06-04 21:51:36 +0300

    sol2-init

---
+++ ch5/solution2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main(void) {
+#if __GNUC__
+    istream& istr = cin;
+
+#else
+    ifstream istr(R"(C:\fedosin\repos\ski\ch5\test.txt)");
+#endif
+    return 0;
+}
