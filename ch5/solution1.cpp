7fde0fb5aba3457b99c99590d17117918b1ea88c
nsdfxela <nsdfxela@gmail.com>
2020-06-04 17:36:44 +0300

    read-data

--- ch5/solution1.cpp
+++ ch5/solution1.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
+
+using namespace std;
+void solve(const string& d1, const string& s2) {
+
+}
 int main(void) {
 #if __GNUC__
     std::istream& istr = std::cin;
 #else
     std::ifstream istr(R"(C:\fedosin\repos\ski\ch5\test.txt)");
 #endif
+    std::string buf, buf1, buf2;
     
+    do {
+        std::getline(istr, buf);
+        if (buf == "0 0") {
+            break;
+        }
+        auto pos = buf.find(' ');
+        buf1 = buf.substr(0, pos);
+        buf2 = buf.substr(pos+1);
+        solve(buf1, buf2);
+    } while (1);
     return 0;
 }
