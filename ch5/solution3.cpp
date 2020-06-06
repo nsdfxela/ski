6e116fabb4e374c10c1b834fd50e20c5feced7b0
nsdfxela <nsdfxela@gmail.com>
2020-06-06 14:36:19 +0300

    ch5-sol3-accepted!

--- ch5/solution3.cpp
+++ ch5/solution3.cpp
@@ -4,40 +4,25 @@
 #include <cmath>
 
 using namespace std;
-int countd(long long int n) {
-    long long int d = n, c = 0;
+
+void solve(int n) {
+    int d = n, c = 0;
     do {
         d = d / 10;
         c++;
     } while (d);
-    return c;
-}
-void solve(int n) {
-    int c = countd(n);
-    int mc = c + 1;
-    long long int md = n;
-    for (int i = 0; i < mc; i++) { md *= 10; }
-    int minp = ceil(log2(md));
-    long long int p = (long long int)1 << minp;
-    int E = minp;
-    while (1) {
-        int cd = countd(p) - c;
-        long long int D = pow(10, cd);
-        long long int rem = p % D;
-        if (p - rem == n * D) {
-            std::cout << E << '\n';
-            break;
-        }
-        else {
-            p = p << 1;
-            E++;
-            if (E == 64) {
-                cout << "no power of 2\n";
-                return;
-            }
+    for (int k = c + 1; k < 1000000000; k++) {
+        double log210 = log2(10);
+        double log2n = log2(n);
+        double log2np1 = log2(n+1);
+
+        int lower = ceil(log2n + k * log210 );
+        int upper = floor(log2np1 + k * log210);
+        for (int l = lower; l < upper+1; l++) {
+            std::cout << l << std::endl;
+            return;
         }
     }
-
 }
 
 int main(void) {
@@ -50,10 +35,11 @@ int main(void) {
     int n;
     while (1) {
         istr >> n;
+        
+        solve(n);
         if (!istr.good()) {
             break;
         }
-        solve(n);
     }
     return 0;
 
