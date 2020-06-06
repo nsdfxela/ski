11ec6926832b5d2142ce870f75c4e03b37a4219f
nsdfxela <nsdfxela@gmail.com>
2020-06-06 12:14:00 +0300

    runtime-err

--- ch5/solution3.cpp
+++ ch5/solution3.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <cmath>
 
 using namespace std;
+int countd(int n) {
+    int d = n, c = 0;
+    do {
+        d = d / 10;
+        c++;
+    } while (d);
+    return c;
+}
 void solve(int n) {
+    int c = countd(n);
+    int mc = c + 1;
+    long long int md = n;
+    for (int i = 0; i < mc; i++) { md *= 10; }
+    int minp = ceil(log2(md));
+    long long int p = (long long int)1 << minp;
+    int E = minp;
+    while (1) {
+        int cd = countd(p) - c;
+        long long int D = pow(10, cd);
+        long long int rem = p % D;
+        if (p - rem == n * D) {
+            std::cout << E << '\n';
+            break;
+        }
+        else {
+            p = p << 1;
+            E++;
+        }
+    }
 
 }
 
