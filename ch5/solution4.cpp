48a48bf49fc90a90b733bc6f01d303cabf2d7025
nsdfxela <nsdfxela@gmail.com>
2020-06-07 12:02:57 +0300

    accepted

--- ch5/solution4.cpp
+++ ch5/solution4.cpp
@@ -2,8 +2,18 @@
 #include <string>
 #include <iostream>
 #include <fstream>
-
+//10127	Ones
 using namespace std;
+
+void solve(long long T) {   
+    long long mod = 1%T;
+    int counter = 1;
+    while (mod) {
+        mod = (mod * 10 + 1) % T;
+        counter++;
+    };
+    std::cout << counter << std::endl;
+}
 
 int main(void) {
 #if __GNUC__
@@ -11,6 +21,13 @@ int main(void) {
 #else
     ifstream istr(R"(C:\fedosin\repos\ski\ch5\test.txt)");
 #endif
-
+    while (1) {
+        long long n;
+        istr >> n;
+        if (!istr.good()) {
+            break;
+        }
+        solve(n);
+    }
     return 0;
 }
