23368ccffae6c6bbc4586374df0b22c5789a9621
nsdfxela <nsdfxela@gmail.com>
2020-06-07 18:24:21 +0300

    initial-dumb-solution

--- ch5/solution5.cpp
+++ ch5/solution5.cpp
@@ -4,9 +4,25 @@
 #include <fstream>
 
 using namespace std;
-
+const string winners[2] = {"Stan wins","Ollie wins"};
 void solve(int n) {
-
+    long long t = 1;
+    int step = 0;
+    while (t < n) {
+        if (step) { t *= 2; }
+        else { t *= 9; }
+        step = !step;
+    }
+    int w1 = !step;
+    t = 1;
+    step = 0;
+    while (t < n) {
+        if (step) { t *= 9; }
+        else { t *= 2; }
+        step = !step;
+    }
+    int w2 = !step;
+    std::cout << winners[w1 * w2] << std::endl;
 }
 
 int main(void) {
