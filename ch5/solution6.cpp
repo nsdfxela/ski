6c018a12836440ec3324b5de50dad2d48f9b33d3
nsdfxela <nsdfxela@gmail.com>
2020-06-08 18:29:34 +0300

    read-data

--- ch5/solution6.cpp
+++ ch5/solution6.cpp
@@ -2,8 +2,14 @@
 #include <fstream>
 #include <cmath>
 #include <string>
+#include <sstream>
+#include <vector>
 
 using namespace std;
+
+void solve(const vector<int>&poly, int n) {
+
+}
 
 int main(void) {
 #if __GNUC__
@@ -11,7 +17,20 @@ int main(void) {
 #else
     ifstream istr(R"(C:\fedosin\repos\ski\ch5\test.txt)");
 #endif
-
+    while (1) {
+        int n, k;
+        istr >> n >> k;
+        if (!istr.good()) {
+            break;
+        }
+        int intb;
+        vector<int> poly;
+        for (int i = 0; i < k; i++) {
+            istr >> intb;
+            poly.push_back(intb);
+        }
+        solve(poly, n);
+    }
 
     return 0;
 }
