1c999dd1e160f6f58f4b158acfc241b793fb89e3
nsdfxela <nsdfxela@gmail.com>
2020-06-12 15:54:39 +0300

    sol7-init

---
+++ ch5/solution7.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+
+//10077 - The Stern - Brocot Number System
+
+using namespace std;
+
+int main(void) {
+#if __GNUC__
+    std::istream& istr = cin;
+#else
+    std::ifstream istr(R"(C:\fedosin\repos\ski\ch5\test.txt)");
+#endif
+
+
+
+    return 0;
+}
