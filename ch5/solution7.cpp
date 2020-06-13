63e50c7be734f608ac550fda577b007ae8a33d65
nsdfxela <nsdfxela@gmail.com>
2020-06-13 12:23:50 +0300

    accepted!

--- ch5/solution7.cpp
+++ ch5/solution7.cpp
@@ -13,7 +13,7 @@ struct f {
     int n;
     int d;
     f(int num, int den) :n{ num }, d{ den } { }
-    f *left = nullptr, *right = nullptr;
+    f* left = nullptr, * right = nullptr;
     void set_left(int num, int den) { left = new f(num, den); }
     void set_right(int num, int den) { right = new f(num, den); }
     ~f() {
@@ -24,18 +24,19 @@ struct f {
 bool operator < (const f& f1, const f& f2) { return (f1.n * f2.d < f1.d* f2.n); }
 bool operator ==(const f& f1, const  f& f2) { return (f1.n * f2.d == f1.d * f2.n); }
 f operator + (const f& f1, const f& f2) { return f(f1.n + f2.n, f1.d + f2.d); }
-void solve(const f &r, const f &s) {
+void solve(const f& r, const f& s) {
     if (s == f(1, 1)) {
         return;
     }
     if (s == r) { return; }
-    if (s < r) { 
+    if (s < r) {
         f new_root = *r.left + r;
         new_root.set_left(r.left->n, r.left->d);
         new_root.set_right(r.n, r.d);
         cout << "L";
         solve(new_root, s);
-    } else { 
+    }
+    else {
         f new_root = *r.right + r;
         new_root.set_left(r.n, r.d);
         new_root.set_right(r.right->n, r.right->d);
@@ -53,7 +54,7 @@ int main(void) {
     while (1) {
         int num, den;
         istr >> num >> den;
-        if (!istr.good()) { break; } 
+        if (!istr.good()) { break; }
         else {
             f s(num, den);
             f root(1, 1);
