a99f976230eb9c2511cf18f18b0067d032d22e41
nsdfxela <nsdfxela@gmail.com>
2020-06-22 21:27:42 +0300

    some-updates

--- ch6/solution1.cpp
+++ ch6/solution1.cpp
@@ -1,10 +1,71 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
+bool _less(string& a, string& b) {
+    if (a.size() < b.size()) {
+        return true;
+    }
+    else if (b.size() < a.size()) {
+        return false;
+    }
+    else {
+        for (int i = a.size() - 1; i >= 0; i --) {
+            if (a[i] - '0' < b[i] - '0') {
+                return true;
+            }
+            else if (a[i] - '0' > b[i] - '0') {
+                return false;
 
+            }
+        }
+    }
+    return false;
+}
+string sum(string& a, string& b) {
+    int ls = a.size() > b.size() ? a.size() : b.size();
+    
+    vector<char> vres;
+    vres.reserve(2 * ls);
+    int rem = 0;
+    for (int i = 0; i < ls; i++) {
+        int idxa = a.size() - 1 - i;
+        int idxb = b.size() - 1 - i;
+        int pa = idxa < 0 ? 0 : a[idxa] - '0';
+        int pb = idxb < 0 ? 0 : b[idxb] - '0';
+        int c = (pa + pb + rem)%10;
+        rem = (pa + pb + rem) / 10;
+        vres.push_back(c + '0');
+    }
+    if(rem){ vres.push_back(rem + '0'); }
+    reverse(vres.begin(), vres.end());
+    vres.push_back('\0');
+    string res(vres.data());
+    return res;
+}
+void inc(string& a) {
+    string one = "1";
+    string nv = sum(a, one);
+    swap(a, nv);
+}
+
+#include <set>
 void solve(string &a, string &b) {
+    set<string> S;
+    string na = "1";
+    string nb = "1";
+    string nc = sum(na, nb);
+    while (_less(nc, b)) {
 
+        S.insert(nc);
+        na = nb;
+        nb = nc;
+        nc = sum(na, nb);
+    };
 }
 
 int main(void) {
