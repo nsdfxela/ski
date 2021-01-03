00adc708bef74eb86339340d3b89b185714254dc
nsdfxela <nsdfxela@gmail.com>
2021-01-03 19:52:04 +0300

    some-bs

--- ch6/solution4.cpp
+++ ch6/solution4.cpp
@@ -7,12 +7,18 @@
 
 
 using namespace std;
-int countCatalans(int n) {
+int countCatalans(int n, int d, int maxd) {
     const int c0 = 1;
-    if (!n) { return c0; }
+    if (!n || d >= maxd) { return c0; }
     int res = 0;
     for (int k = 0; k <= n - 1; k++) {
-        res += countCatalans(k) * countCatalans(n-1-k);
+        int a=1, b=1;
+        if (d < maxd-1)
+        {
+            a = countCatalans(k, d + 1, maxd);
+            b = countCatalans(n - 1 - k, d + 1, maxd);
+        }
+        res += a * b;
     }
     return res;
 }
@@ -23,7 +29,7 @@ void solve(int n, int d) {
 
 }
 int main(void) {
-    std::cout << countCatalans(3) << "\n";
+    std::cout << countCatalans(300, 0, 150) << "\n";
 #if __GNUC__
     istream& istr = cin;
 #else
