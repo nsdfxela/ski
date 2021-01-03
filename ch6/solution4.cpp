b9250b59848f894bfc089a544e146919e7052def
nsdfxela <nsdfxela@gmail.com>
2021-01-03 19:34:16 +0300

    some-catalans-counting

--- ch6/solution4.cpp
+++ ch6/solution4.cpp
@@ -7,12 +7,23 @@
 
 
 using namespace std;
+int countCatalans(int n) {
+    const int c0 = 1;
+    if (!n) { return c0; }
+    int res = 0;
+    for (int k = 0; k <= n - 1; k++) {
+        res += countCatalans(k) * countCatalans(n-1-k);
+    }
+    return res;
+}
 void solve(int n, int d) {
-
-
+    //n 2-300   
+    //d 1-150
+    
 
 }
 int main(void) {
+    std::cout << countCatalans(3) << "\n";
 #if __GNUC__
     istream& istr = cin;
 #else
@@ -23,7 +34,7 @@ int main(void) {
         getline(istr, buffer);
         if (!istr.good()) {
             break;
-        }
+        } 
         if (!buffer.empty()) {
             int n, d;
             stringstream ss(buffer);
