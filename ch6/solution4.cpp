a372d7a54f3a6d9f865a406af7920bd626eacadf
nsdfxela <nsdfxela@gmail.com>
2021-01-04 13:05:33 +0300

    need-big-numbers

--- ch6/solution4.cpp
+++ ch6/solution4.cpp
@@ -9,30 +9,36 @@
 
 using namespace std;
 unordered_map<int, int> mem;
-int countCatalans(int n) {
+int countCatalans(int n, int d, int maxd) {
     const int c0 = 1;
-    if (!n ) { return c0; }
+    if (!n || d >= maxd-1) { return c0; }
     if (mem.find(n) != mem.end()) 
     {
         return mem[n];
     }
     int res = 0;
     for (int k = 0; k <= n - 1; k++) {
-        int a = countCatalans(k);
-        int b = countCatalans(n-1-k);
+        int a = countCatalans(k, d++, maxd);
+        int b = countCatalans(n-1-k, d++, maxd);
         res += a * b;
+        //std::cout << res << "\n";
     }
     mem[n] = res;
     return res;
 }
 void solve(int n, int d) {
-    //n 2-300   
-    //d 1-150
+    int nr = n % 2;
+    if (nr) {
+        std::cout << 0 << '\n';
+    }
+    else 
+    {
+        std::cout << countCatalans(n / 2, 0, d) << '\n';
+    }
     
-
 }
 int main(void) {
-    std::cout << countCatalans(300) << "\n";
+    //std::cout << countCatalans(3, 0, 2) << "\n";
 #if __GNUC__
     istream& istr = cin;
 #else
