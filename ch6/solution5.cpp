a33da70e0650e3e9384ebe8f9d2237e6553af7ab
nsdfxela <nsdfxela@gmail.com>
2021-01-08 13:25:20 +0300

    gnuc

--- ch6/solution5.cpp
+++ ch6/solution5.cpp
@@ -8,8 +8,11 @@ void solve(int k, int d) {
 }
 int main(int argc, char**argv) {
 
+#if __GNUC__
+    std::istream& istr = std::cin;
+#else
     std::ifstream istr (R"(C:\fedosin\repos\ski\ch6\test5.txt)");
-    
+#endif
     int k, d;
     while (true) {
         istr >> k >> d;
