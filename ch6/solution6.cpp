ec87af9a7c447ddcef560345cfb9cb01d37e3ac7
nsdfxela <nsdfxela@gmail.com>
2021-01-21 22:33:18 +0300

    simple-hanoi

--- ch6/solution6.cpp
+++ ch6/solution6.cpp
@@ -5,13 +5,22 @@
 #include <vector>
 #include <fstream>
 
+int cnt = 0;
+void basicHanoi(int n, int from, int to, int buffer) {
+    if (!n) { return; }
+    basicHanoi(n-1, from, buffer, to);
+    std::cout << cnt++ << ": " <<  from << "->" << to << '\n';
+    
+    basicHanoi(n - 1, buffer, to, from);
+}
+
 void solve(int n) {
 
 
 }
 
 int main() {
-
+    basicHanoi(8, 0, 2, 1);
 #if __GNUC__
     std::istream& istr = std::cin;
 #else
