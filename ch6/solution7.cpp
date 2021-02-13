9acb4e9661c7a7add3ae0132db8b325e106a699b
nsdfxela <nsdfxela@gmail.com>
2021-02-13 20:08:23 +0300

    naive-golomb

--- ch6/solution7.cpp
+++ ch6/solution7.cpp
@@ -4,11 +4,29 @@
 #include <vector>
 #include <fstream>
 
+const int NMAX = 2000000000;
+
+void naiveGolomb() {
+    static std::vector<int> G;
+    if (!G.empty()) { return; }
+    G.push_back(1);
+    G.push_back(2);
+
+    int ival = 2;
+
+    for (; ival < NMAX; ival++) {
+        for (int i = 0; i < G[ival-1]; i++) {
+            G.push_back(ival);
+        }
+    }
+}
+
 void solve(int n) {
 
 }
 
 int main() {
+    naiveGolomb();
 #if __GNUC__
     std::istream &istr = std::cin;
 #else
