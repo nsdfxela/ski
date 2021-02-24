c5773e0a5ac89479ed0fa75fbbda5228dfe36b0f
nsdfxela <nsdfxela@gmail.com>
2021-02-24 13:20:11 +0300

    time-limit

--- ch7/solution1.cpp
+++ ch7/solution1.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <cmath>
+#include <set>
 
 // 10110 - Light, more light
 // https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=35&page=show_problem&problem=1051
@@ -8,11 +10,31 @@
 typedef unsigned long int mint;
 
 void solve(mint v) {
-
+    auto s = (mint)ceill(std::sqrt(v));
+    std::set<mint> dvs;
+    dvs.insert(1);
+    int t = v;
+    for (mint i = 2; i <= s && t != 1; i++) {
+        while (t % i==0) {
+            t = t / i;
+            dvs.insert(i);
+        }
+    }
+    if (v != 1)
+    {
+        dvs.insert(v);
+    }
+    if (dvs.size() % 2) {
+        std::cout << "yes";
+    }
+    else {
+        std::cout << "no";
+    }
+    std::cout << '\n';
 
 }
 int main(void) {
-
+    solve(2147483648ul);
 #if __GNUC__
     std::istream& istr = std::cin;
 #else
